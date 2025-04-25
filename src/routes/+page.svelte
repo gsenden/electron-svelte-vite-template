89d6ea9464bc14127c57563f7939af253206d9e6
Ger Senden <gsenden@outlook.com>
2025-04-25 15:46:04 +0200

    Added a tailwind screen to test if it is working

--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -1,2 +1,29 @@
-<h1>Welcome to SvelteKit</h1>
-<p>Visit <a href="https://svelte.dev/docs/kit">svelte.dev/docs/kit</a> to read the documentation</p>
+
+<div
+    class="
+      grid
+      grid-cols-[min-content_1fr_min-content]
+      gap-8             /* 2rem gap on both axes */
+      bg-gray-100
+      p-4
+      border
+      border-gray-300
+    "
+  >
+    <!-- Left column (auto-width) -->
+    <div class="bg-red-300 p-4">Left</div>
+
+    <!-- Middle column fills remaining space -->
+    <div class="min-w-0 bg-green-300 p-4">
+      <input
+        type="text"
+        class="w-full border border-gray-500 p-2"
+        placeholder="I fill the gap"
+      />
+    </div>
+
+    <!-- Right column (auto-width) -->
+    <div class="bg-blue-300 p-4">Right</div>
+
+
+  </div>
